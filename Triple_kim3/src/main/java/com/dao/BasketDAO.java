22d44530b265f29de0faf5b89a2f69ed0665c914
incheon123 <xorgus704@gmail.com>
2023-11-26 00:42:08 +0900

    sql문 변경

--- Triple_kim3/src/main/java/com/dao/BasketDAO.java
+++ Triple_kim3/src/main/java/com/dao/BasketDAO.java
@@ -23,12 +23,19 @@ public class BasketDAO {
 		this.conn = conn;
 	}
 	
-	public boolean insertByPid(int pid, int quantity){
+	public boolean insertByPid(String uid, String pid, int size, int quantity){
 		try {
 			
-			pstmt = conn.prepareStatement("insert into basket values(?, ?)" );
-			pstmt.setInt(1, pid);
-			pstmt.setInt(2, quantity);
+			pstmt = conn.prepareStatement("INSERT INTO CART values(?, ?, ?, ?)" );
+			
+			pstmt.setString(1, uid);
+			pstmt.setString(2, pid);
+			pstmt.setInt(3, size);
+			pstmt.setInt(4, quantity);
+			
+			
+			//추가하기전에 pk위반사항 검사하고 위반되면 에러보내기
+			
 			pstmt.execute();
 			
 			//성공적으로 삽입시
